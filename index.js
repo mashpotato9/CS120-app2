86905018fc097631e6b784002c10b3e64453713d
mashpotato9 <127051168+mashpotato9@users.noreply.github.com>
2024-08-04 22:45:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/router');
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +10,6 @@ app.use(router);
 
 app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is listening on port 3000');
 });
